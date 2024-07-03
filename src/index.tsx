2bae77bd988b4ded48bbef1a0b5c8e2a9149c0e2
emuri <yuanshanzhen@gmail.com>
2024-07-03 21:34:49 +0900

    index.tsxに変更

--- src/index.tsx
+++ src/index.tsx
@@ -4,72 +4,74 @@ import { serve } from '@hono/node-server'
 
 const app = new Hono()
 
-const View = () => {
-        return (
-        <html>
-        <html lang="en"/>
-        <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-        </head>
-        <body>
-        <h1>hello</h1>
-        </body>
-        </html>
-        )
-}
+app.get('/' , (c) => c.text('hello'))
 
-app.use(
-  '/auth/*',
-  basicAuth(
-    {
-      username: 'hono',
-      password: 'acoolproject',
-      // Define other params in the first object
-      realm: 'www.example.com',
-    },
-    {
-      username: 'hono-admin',
-      password: 'super-secure',
-      // Cannot redefine other params here
-    },
-    {
-      username: 'hono-user-1',
-      password: 'a-secret',
-      // Or here
-    }
-  )
-)
+// const View = () => {
+//         return (
+//         <html>
+//         <html lang="en"/>
+//         <head>
+//         <meta charset="UTF-8" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//         <title>Document</title>
+//         </head>
+//         <body>
+//         <h1>hello</h1>
+//         </body>
+//         </html>
+//         )
+// }
 
-app.get('/auth' , (c) => {
-  return c.text('your are authorized')
-})
+// app.use(
+//   '/auth/*',
+//   basicAuth(
+//     {
+//       username: 'hono',
+//       password: 'acoolproject',
+//       // Define other params in the first object
+//       realm: 'www.example.com',
+//     },
+//     {
+//       username: 'hono-admin',
+//       password: 'super-secure',
+//       // Cannot redefine other params here
+//     },
+//     {
+//       username: 'hono-user-1',
+//       password: 'a-secret',
+//       // Or here
+//     }
+//   )
+// )
 
-app.get('/', (c) => {
-  return c.text('hello.node.js')
-})
-
-app.get('/page', (c) => {
-  return c.html(<View/>)
-})
-
-// app.get('/', (c) => {
-//   return c.text('HOno!')
+// app.get('/auth' , (c) => {
+//   return c.text('your are authorized')
 // })
 
-app.get('posts/:id' , (c) => {
-  const page = c.req.query('page')
-  const id = c.req.param('id')
-  c.header('MEssage' , 'Hi')
-  return c.text(`you want to see page ${page} of post ${id}`)
-})
+// app.get('/', (c) => {
+//   return c.text('hello.node.js')
+// })
 
-app.get('/api/hello', (c) => {
-  return c.json({
-    ok: true,
-    message: 'hello hono!',
-  })
-})
+// app.get('/page', (c) => {
+//   return c.html(<View/>)
+// })
+
+// // app.get('/', (c) => {
+// //   return c.text('HOno!')
+// // })
+
+// app.get('posts/:id' , (c) => {
+//   const page = c.req.query('page')
+//   const id = c.req.param('id')
+//   c.header('MEssage' , 'Hi')
+//   return c.text(`you want to see page ${page} of post ${id}`)
+// })
+
+// app.get('/api/hello', (c) => {
+//   return c.json({
+//     ok: true,
+//     message: 'hello hono!',
+//   })
+// })
 
 export default app
